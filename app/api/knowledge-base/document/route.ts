128c8886d3c88959bbe70cecec1e2bd826ce98e5
levin <30230347+levin8023@users.noreply.github.com>
2024-10-16 17:37:31 +0800

    refactor: document route

--- app/api/knowledge-base/document/route.ts
+++ app/api/knowledge-base/document/route.ts
@@ -1,34 +1,8 @@
-import { TextLoader } from 'langchain/document_loaders/fs/text'
 import { RecursiveCharacterTextSplitter } from 'langchain/text_splitter'
 import { getVectorStore } from '@/app/provider/vector_store'
-import { PDFLoader } from '@langchain/community/document_loaders/fs/pdf'
-import { DocxLoader } from '@langchain/community/document_loaders/fs/docx'
-import { CSVLoader } from '@langchain/community/document_loaders/fs/csv'
-import { JSONLoader } from 'langchain/document_loaders/fs/json'
 import { pool } from '@/app/provider/pg'
-import { randomUUID } from 'node:crypto'
 import { NextResponse } from 'next/server'
-
-function getFileLoader (file: File) {
-  let name = file.name
-  console.log('getFileLoader, file name: ', name)
-  if (name.endsWith('.txt')) {
-    return new TextLoader(file)
-  }
-  if (name.endsWith('.pdf')) {
-    return new PDFLoader(file)
-  }
-  if (name.endsWith('.docx')) {
-    return new DocxLoader(file)
-  }
-  if (name.endsWith('.csv')) {
-    return new CSVLoader(file)
-  }
-  if (name.endsWith('.json')) {
-    return new JSONLoader(file)
-  }
-  return new TextLoader(file)
-}
+import { getFileLoader, saveDocumentFile } from '@/app/provider/document'
 
 export async function POST (request: Request) {
   try {
@@ -58,31 +32,6 @@ export async function POST (request: Request) {
     console.error('Error processing request:', error)
     return new Response('Internal Server Error', { status: 500 })
   }
-}
-
-export async function saveDocumentFile (file: File) {
-  await createDocumentTable()
-  const sql = 'INSERT INTO knowledge_base_document (id, name, type, content) ' +
-    'VALUES ($1, $2, $3, $4) RETURNING id'
-  const fileContent = await file.arrayBuffer()
-  const buffer = Buffer.from(fileContent)
-  const result = await pool.query(sql,
-    [randomUUID(), file.name, file.type, buffer])
-  return result.rows[0].id
-}
-
-async function createDocumentTable () {
-  const sql = `
-      CREATE TABLE IF NOT EXISTS knowledge_base_document
-      (
-          id         varchar(36) PRIMARY KEY,
-          name       TEXT NOT NULL,
-          type       TEXT NULL,
-          content BYTEA NOT NULL,
-          created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-      );
-  `
-  await pool.query(sql)
 }
 
 export async function DELETE (request: Request) {
